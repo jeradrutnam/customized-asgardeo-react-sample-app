9d58302d68a96fa90522694b9ce3bd26b90c81f8
jeradrutnam <jeradrutnam@outlook.com>
2022-04-01 20:41:58 +0530

    Add authentication endpoint

---
+++ customized-asgardeo-react-sample-app/src/pages/home.tsx
@@ -0,0 +1,211 @@
+/**
+ * Copyright (c) 2021, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import { useAuthContext } from "@asgardeo/auth-react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import React, { FunctionComponent, ReactElement, useEffect, useState } from "react";
+import { default as authConfig } from "../config.json";
+import HOME_BACKGROUND from "../images/home-bg.jpg";
+import COMPANY_LOGO from "../images/logo.png";
+import COMPANY_LOGO_INVERSE from "../images/logo-inverse.png";
+import FOOTER_LOGO from "../images/footer-logo.png";
+import { DefaultLayout } from "../layouts/default";
+
+/**
+ * Decoded ID Token Response component Prop types interface.
+ */
+type HomePagePropsInterface = {};
+
+/**
+ * Home page for the Sample.
+ *
+ * @param {HomePagePropsInterface} props - Props injected to the component.
+ *
+ * @return {React.ReactElement}
+ */
+export const HomePage: FunctionComponent<HomePagePropsInterface> = (
+    props: HomePagePropsInterface
+): ReactElement => {
+
+    const {
+        state,
+        signIn,
+        signOut,
+        getBasicUserInfo,
+        getIDToken,
+        getDecodedIDToken
+    } = useAuthContext();
+
+    const [ derivedAuthenticationState, setDerivedAuthenticationState ] = useState<any>(null);
+    const [ hasAuthenticationErrors, setHasAuthenticationErrors ] = useState<boolean>(false);
+
+    useEffect(() => {
+
+        if (!state?.isAuthenticated) {
+            return;
+        }
+
+        (async (): Promise<void> => {
+            const basicUserInfo = await getBasicUserInfo();
+            const idToken = await getIDToken();
+            const decodedIDToken = await getDecodedIDToken();
+
+            const derivedState = {
+                authenticateResponse: basicUserInfo,
+                idToken: idToken.split("."),
+                decodedIdTokenHeader: JSON.parse(atob(idToken.split(".")[0])),
+                decodedIDTokenPayload: decodedIDToken
+            };
+
+            setDerivedAuthenticationState(derivedState);
+            console.log(derivedState.decodedIDTokenPayload);
+        })();
+    }, [ state.isAuthenticated ]);
+
+    const handleLogin = () => {
+        signIn()
+            .catch(() => setHasAuthenticationErrors(true));
+    };
+
+    const handleLogout = () => {
+        signOut();
+    };
+
+    const handleDropdownOnClick = (e) => {
+        e.target.classList.toggle("show");
+    };
+
+    // If `clientID` is not defined in `config.json`, show a UI warning. 
+    if (!authConfig?.clientID) {
+
+        return (
+            <div className="content">
+                <h2>You need to update the Client ID to proceed.</h2>
+                <p>Please open "src/config.json" file using an editor, and update
+                    the <code>clientID</code> value with the registered application's client ID.</p>
+                <p>Visit repo <a
+                    href="https://github.com/asgardeo/asgardeo-auth-react-sdk/tree/master/samples/asgardeo-react-app">README</a> for
+                    more details.</p>
+            </div>
+        );
+    }
+
+    return (
+        <DefaultLayout
+            isLoading={ state.isLoading }
+            hasErrors={ hasAuthenticationErrors }
+        >
+            {
+                state.isAuthenticated
+                    ? (
+                        <>
+                            <div className="header">
+                                <img src={ COMPANY_LOGO } className="company-logo-image logo"/>
+                                <ul className="nav">
+                                    <li>Online Certificates</li>
+                                    <li>Group Programs</li>
+                                    <li>Keynotes</li>
+                                    <li className="dropdown dropdown-toggle" onClick={ (e) => { handleDropdownOnClick(e) } }>
+                                        Welcome { derivedAuthenticationState?.decodedIDTokenPayload.given_name || derivedAuthenticationState?.decodedIDTokenPayload.username }
+                                        <ul className="dropdown-menu text-small show">
+                                            <li><a className="dropdown-item" onClick={ () => { handleLogout() } }>Sign out</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <FontAwesomeIcon icon={ faMagnifyingGlass } />
+                                    </li>
+                                </ul>
+                            </div>
+                            <div className="content">
+                                <div className="container">
+                                    <div className="grid">
+                                        <div className="column side-nav-section">
+                                            <ul className="side-nav">
+                                                <li className="active">Student Profile</li>
+                                                <li>Certifications</li>
+                                                <li>Announcements</li>
+                                                <li>Events</li>
+                                                <li>Help</li>
+                                            </ul>
+                                        </div>
+                                        <div className="column">
+                                            <h1 className="page-title">Student Profile</h1>
+                                            <hr className="page-title-underline" />
+                                            <div>
+                                                <form>
+                                                    <h2>Personal Information</h2>
+                                                    <div className="field">
+                                                        <label>First Name :</label>
+                                                        <input type="text" readOnly value={ derivedAuthenticationState?.decodedIDTokenPayload.given_name } />
+                                                    </div>
+                                                    <div className="field">
+                                                        <label>Date of Birth :</label>
+                                                        <input type="text" readOnly value={ derivedAuthenticationState?.decodedIDTokenPayload.birthdate } />
+                                                    </div>
+                                                </form>
+                                            </div>
+                                        </div>
+                                        <div className="column">
+                                            <div className="section-box">
+                                                <div className="section-box-header">Events</div>
+                                                <div className="section-box-content"></div>
+                                            </div>
+                                            <div className="section-box">
+                                                <div className="section-box-header">Announcements</div>
+                                                <div className="section-box-content"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <footer>
+                                <div className="container">
+                                    <div className="left">
+                                        <img src={ FOOTER_LOGO } className="footer-logo-image logo"/>
+                                        <ul className="footer-links">
+                                            <li>About ABC University</li>
+                                            <li>Contact us</li>
+                                            <li>Student Alumni</li>
+                                            <li>University Course Guide</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </footer>
+                        </>
+                    )
+                    : (
+                        <div className="content flex-center home-page-content" style={ { backgroundImage: `url(${HOME_BACKGROUND})`} }>
+                            <img src={ COMPANY_LOGO_INVERSE } className="company-logo-image logo"/>
+                            <h1 className="home-description">
+                                Welcome to ABC Uni Student Portal. Please login to proceed.
+                            </h1>
+                            <button
+                                className="btn primary home-button"
+                                onClick={ () => {
+                                    handleLogin();
+                                } }
+                            >
+                                Login
+                            </button>
+                        </div>
+                    )
+            }
+        </DefaultLayout>
+    );
+};
---
+++ customized-wso2-identity-server-authentication-endpoint/authenticationendpoint/includes/header.jsp
@@ -0,0 +1,154 @@
+<%--
+  ~ Copyright (c) 2019, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+  ~
+  ~ WSO2 Inc. licenses this file to you under the Apache License,
+  ~ Version 2.0 (the "License"); you may not use this file except
+  ~ in compliance with the License.
+  ~ You may obtain a copy of the License at
+  ~
+  ~    http://www.apache.org/licenses/LICENSE-2.0
+  ~
+  ~ Unless required by applicable law or agreed to in writing,
+  ~ software distributed under the License is distributed on an
+  ~ "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+  ~ KIND, either express or implied.  See the License for the
+  ~ specific language governing permissions and limitations
+  ~ under the License.
+  --%>
+
+<%@ include file="localize.jsp" %>
+<%@ page import="org.wso2.carbon.identity.application.authentication.endpoint.util.AuthenticationEndpointUtil" %>
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<link rel="icon" href="libs/themes/default/assets/images/favicon.ico" type="image/x-icon"/>
+<link href="libs/themes/default/theme.min.css" rel="stylesheet">
+<link href="libs/fontawesome-free-6.1.1-web/css/all.css" rel="stylesheet">
+
+<style type="text/css">
+
+a {
+    color: #bc261a;
+}
+
+.width-100 {
+    width: 100%;
+}
+
+hr.page-title-underline {
+    width: 40px;
+    margin: 0 auto;
+    height: 3px;
+    background-color: #bc261a;
+    margin-bottom: 30px;
+}
+
+footer {
+    background: #111;
+    color: #fff;
+}
+
+.ui.primary.button, .ui.primary.buttons .button {
+    background-color: #bc261a;
+}
+
+.ui.button.link-button {
+    color: #bc261a;
+}
+
+.ui.primary.button:hover, .ui.primary.buttons .button:hover {
+    background-color: #6f0800;
+}
+
+.ui.segment {
+    background-color: #f1f1f1;
+}
+
+.hide {
+  display: none !important;
+}
+
+.header:not(.ui) {
+    background-color: #333333;
+    color: #ffffff;
+    padding: 20px;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
+}
+
+.header:not(.ui) > .company-logo-image {
+    height: 40px;
+    margin-right: auto;
+}
+
+.header:not(.ui) > ul.nav {
+    display: flex;
+    flex-wrap: wrap;
+    list-style: none;
+    margin: 8px 0;
+    text-transform: uppercase;
+}
+
+.header:not(.ui) > ul.nav > li {
+    margin: 0 10px;
+    cursor: pointer;
+}
+
+.header:not(.ui) > ul.nav > li:hover {
+    color: #bc261a;
+}
+
+.dropdown {
+    position: relative;
+    margin: 0 20px !important;
+}
+
+.dropdown.show > .dropdown-menu {
+    display: block;
+}
+
+.dropdown-toggle {
+    cursor: pointer;
+}
+
+.dropdown-toggle::after {
+    display: inline-block;
+    margin-left: 0.255em;
+    vertical-align: 0.255em;
+    content: "";
+    border-top: 0.3em solid;
+    border-right: 0.3em solid transparent;
+    border-bottom: 0;
+    border-left: 0.3em solid transparent;
+}
+
+.dropdown-menu {
+    background: #111;
+    color: #fff;
+    position: absolute;
+    inset: 0px auto auto 0px;
+    margin: 0;
+    padding: 0;
+    list-style: none;
+    min-width: 100%;
+    transform: translate3d(0px, 34px, 0px);
+    display: none;
+}
+
+.dropdown-menu a {
+    color: #fff;
+    text-decoration: none;
+    cursor: pointer;
+    font-size: 14px;
+    width: 100%;
+    padding: 10px 0;
+    display: block;
+}
+</style>
+
+<title><%=AuthenticationEndpointUtil.i18n(resourceBundle, "wso2.identity.server")%></title>
+
+<script src="libs/jquery_3.4.1/jquery-3.4.1.js"></script>
